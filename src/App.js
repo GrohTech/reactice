ad70c9e88206763b2c0e420b046958300e2517dd
Lauren Groh <legroh@uwm.edu>
2022-03-12 20:55:34 -0600

    import components to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Scout from './components/Scout';
+import Fidget from './components/Fidget';
+import Alice from './components/Alice';
+import Lucy from './components/Lucy';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scout />
+      <Fidget />
+      <Alice />
+      <Lucy />
     </div>
   );
 }
